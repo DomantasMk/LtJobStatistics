2bffddf064ab8ca4c097e3a5b16f834cb60a05d8
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-05-09 23:08:24 +0300

    Fixed axios

--- client/src/components/ColumnChart.js
+++ client/src/components/ColumnChart.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactApexChart from 'react-apexcharts';
+import axios from 'axios';
 export default class ColumnChart extends React.Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-
-            series: [{
-                data: [100, 22, 10, 28, 16, 21, 13, 30] //Series of data for the chart
-            }],
+            series: [
+                {
+                    data: [100, 22, 10, 28, 16, 21, 13, 30], //Series of data for the chart
+                },
+            ],
             options: {
                 chart: {
                     height: 350,
@@ -17,23 +18,24 @@ export default class ColumnChart extends React.Component {
                     events: {
                         click: function (chart, w, e) {
                             // console.log(chart, w, e)
-                        }
-                    }
+                        },
+                    },
                 },
-                colors:['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
+                colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
                 plotOptions: {
                     bar: {
                         columnWidth: '45%',
-                        distributed: true
-                    }
+                        distributed: true,
+                    },
                 },
                 dataLabels: {
-                    enabled: false
+                    enabled: false,
                 },
                 legend: {
-                    show: false
+                    show: false,
                 },
-                xaxis: { //series of data labels for the chart
+                xaxis: {
+                    //series of data labels for the chart
                     categories: [
                         'Dick',
                         'Javascript',
@@ -46,22 +48,40 @@ export default class ColumnChart extends React.Component {
                     ],
                     labels: {
                         style: {
-                            colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
-                            fontSize: '12px'
-                        }
-                    }
-                }
+                            colors: [
+                                '#2E93fA',
+                                '#66DA26',
+                                '#546E7A',
+                                '#E91E63',
+                                '#FF9800',
+                            ],
+                            fontSize: '12px',
+                        },
+                    },
+                },
             },
         };
     }
-    componentDidMount(){
-        fetch('http://localhost:5000/api/main/Technologies/1').then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err));
+    componentDidMount() {
+        axios
+            .get(`/api/main/Technologies/1`)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     render() {
         return (
-            <div id="chart">
-                <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
+            <div id='chart'>
+                <ReactApexChart
+                    options={this.state.options}
+                    series={this.state.series}
+                    type='bar'
+                    height={350}
+                />
             </div>
         );
     }
