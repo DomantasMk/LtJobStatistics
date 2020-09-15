471e23f19f3729c1bcc322c9b6aa6c2dac870efc
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-09-15 18:19:57 +0300

    Update on dependencies + updated pie chart

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 # LtItJobStatistics
+
 This website was made to research the trends of technologies/tools mentioned in IT job adverts. You can see generated charts from data we gathered, the default charts are of top 10 technologies in the database, but there is a filter if you want to view different data, feel free to check out 😎.
 
 ![](header.png)
 
-# Link to live version 
+# Link to live version
+
 https://ltitjobstatistics.herokuapp.com
 
 # For this project we used:
 
-* Client side - React.js
-* Server side - Node.js
-* Scraping data - Puppeter library
-* Database - MongoDB/Mongoose
-* Node.js Framework - Express.js
-
+- Client side - React.js
+- Server side - Node.js
+- Scraping data - Puppeter library
+- Database - MongoDB/Mongoose
+- Node.js Framework - Express.js
 
 ## Installation
 
 ```sh
 npm install
-npm start
+npm run client-install
+npm run dev
 ```
 
 ## Meta
 
 Domantas Mikelionis - https://github.com/DomantasMk / https://www.linkedin.com/in/domantas-mikelionis-36aa84172/  
-Tautvydas Tutis - https://github.com/TautvydasGustas1 / https://www.linkedin.com/in/tautvydas-gustas/
+Tautvydas Gustas - https://github.com/TautvydasGustas1 / https://www.linkedin.com/in/tautvydas-gustas/
 
 ## Contributing
 
@@ -34,4 +36,3 @@ Tautvydas Tutis - https://github.com/TautvydasGustas1 / https://www.linkedin.com
 3. Commit your changes (`git commit -am 'Add some fooBar'`)
 4. Push to the branch (`git push origin feature/fooBar`)
 5. Create a new Pull Request
-
--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -1,47 +1,47 @@
-import React, { useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
+import PropTypes from "prop-types";
 
 const PieChart = ({ counts, titles }) => {
-    const [chartState, setChartState] = useState({
-        series: counts,
-        options: {
+  const [chartState, setChartState] = useState({
+    series: counts,
+    options: {
+      chart: {
+        width: 380,
+        type: "pie",
+      },
+      labels: titles,
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
             chart: {
-                width: 380,
-                type: 'pie',
+              width: 320,
             },
-            labels: titles,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 200,
-                        },
-                        legend: {
-                            position: 'bottom',
-                        },
-                    },
-                },
-            ],
+            legend: {
+              position: "bottom",
+            },
+          },
         },
-    });
+      ],
+    },
+  });
 
-    return (
-        <div id='pieChart'>
-            <ReactApexChart
-                options={chartState.options}
-                series={chartState.series}
-                type='pie'
-                width={380}
-            />
-        </div>
-    );
+  return (
+    <div id="pieChart">
+      <ReactApexChart
+        options={chartState.options}
+        series={chartState.series}
+        type="pie"
+        width={380}
+      />
+    </div>
+  );
 };
 
 PieChart.propTypes = {
-    counts: PropTypes.array.isRequired,
-    titles: PropTypes.array.isRequired,
+  counts: PropTypes.array.isRequired,
+  titles: PropTypes.array.isRequired,
 };
 
 export default PieChart;
