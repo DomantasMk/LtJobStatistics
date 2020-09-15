aa9924a64a1fcacf1d604ff7776317100232ef9c
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-09-15 18:58:24 +0300

    Added background to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,54 +1,51 @@
 import React from "react";
-import {
-  Container,
-  Box,
-  Typography,
-  IconButton,
-  Grid,
-  Tooltip,
-} from "@material-ui/core";
+import { Box, Typography, IconButton, Grid, Tooltip } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
+const boxStyle = {
+  background:
+    "linear-gradient(180deg, rgba(255,255,255,0) 32%, rgba(0,143,251,1) 100%)",
+  textAlign: "center",
+};
+
 const Footer = (props) => {
   return (
-    <Container maxWidth={"lg"}>
-      <Box style={{ textAlign: "center" }} mt={8} mb={5}>
-        <Typography variant={"h6"}>
-          You can find code and developers here :)
-        </Typography>
-        <Box mt={2} mb={3}>
-          <Tooltip title="Project repository">
-            <IconButton
-              href="https://github.com/DomantasMk/LtJobStatistics"
-              target="_blank"
-            >
-              <GitHubIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Domantas linked in profile">
-            <IconButton
-              href="https://www.linkedin.com/in/domantas-mikelionis-36aa84172/"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Tautvydas linked in profile">
-            <IconButton
-              href="https://www.linkedin.com/in/tautvydas-gustas/"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </IconButton>
-          </Tooltip>
-        </Box>
-        <Grid></Grid>
-        <Typography variant={"body1"}>
-          © LTJobStatistics {new Date().getFullYear()}
-        </Typography>
+    <Box style={boxStyle} width="100%" mt={7}>
+      <Typography variant={"h6"}>
+        You can find code and developers here :)
+      </Typography>
+      <Box mt={2} mb={3}>
+        <Tooltip title="Project repository">
+          <IconButton
+            href="https://github.com/DomantasMk/LtJobStatistics"
+            target="_blank"
+          >
+            <GitHubIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Domantas linked in profile">
+          <IconButton
+            href="https://www.linkedin.com/in/domantas-mikelionis-36aa84172/"
+            target="_blank"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Tautvydas linked in profile">
+          <IconButton
+            href="https://www.linkedin.com/in/tautvydas-gustas/"
+            target="_blank"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Tooltip>
       </Box>
-    </Container>
+      <Grid></Grid>
+      <Typography variant={"body1"}>
+        © LTJobStatistics {new Date().getFullYear()}
+      </Typography>
+    </Box>
   );
 };
 
